014e81d1af3bf5851be4a3e8ceffd695dbb52fa8
David Hillesheim <davidhillesheim6@gmail.com>
2026-02-17 19:10:54 -0300

    Aulas 1 - 10pt1 assistidas.

---
+++ src/br/com/dvdh/maratonadevdojo/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,18 @@
+package br.com.dvdh.maratonadevdojo.introducao;
+
+/**
+ *  Isso é um comentário <b>javadoc</b>
+ */
+//Isso explica muita coisa.
+
+public class Aula01OlaDevDojo {
+    //Isso é um comentário de uma linha
+    /*
+        Isso é um comentário de
+        múltiplas
+        linhas
+     */
+    public static void main() {
+        System.out.println("Olá Mundo!");
+    }
+}
---
+++ src/br/com/dvdh/maratonadevdojo/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package br.com.dvdh.maratonadevdojo.introducao;
+
+public class Aula02TiposPrimitivos {
+    static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        System.out.println("A idade é: " + age + " anos");
+    }
+}
